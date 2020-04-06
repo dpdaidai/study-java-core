40462e0dd64f9208c9958cf02ad4494e445c0226
cpt at mac <ks55259980@163.com>
2020-04-06 11:09:59 +0800

    add SimpleClassStructure

---
+++ src/main/java/com/core/java/v1ch03/SimpleClassStructure.java
@@ -0,0 +1,30 @@
+package com.core.java.v1ch03;
+
+/**
+ * @Author chenpantao
+ * @Date 2020-04-06 10:53
+ * @Version 1.0
+ */
+/**
+ * access modifier 访问修饰符
+ * 关键字 class 表明 java 程序中全部内容包含在类中
+ * 类名规则 : 以字母开头 , 不可以使用保留字 , 建议使用驼峰结构
+ */
+public class SimpleClassStructure {
+
+    /**
+     * 运行已编译的程序时 , java 虚拟机从指定类中的 main 方法开始执行
+     * 该方法没有返回值 , 正常退出则退出代码为0 .
+     * 如果希望在终止程序时返回其它代码 , 调用 System.exit 方法.
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        /**
+         * Java 通用语法 : object.method(parameters)
+         * 在这里 ,System.out 对象并调用了它的 println 方法.
+         */
+        System.out.println("First Sample");
+
+    }
+}
--- src/test/java/com/core/java/StudyJavaCoreApplicationTests.java
+++ src/test/java/com/core/java/StudyJavaCoreApplicationTests.java
@@ -5,12 +5,37 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Calendar;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StudyJavaCoreApplicationTests {
 
     @Test
     public void contextLoads() {
+
+        Calendar instance = Calendar.getInstance();
+        int weekYear = instance.getWeekYear();
+        System.out.println(weekYear);
+        int weeksInWeekYear = instance.getWeeksInWeekYear();
+        System.out.println(weeksInWeekYear);
+        int firstDayOfWeek = instance.getFirstDayOfWeek();
+        System.out.println(firstDayOfWeek);
+        int minimalDaysInFirstWeek = instance.getMinimalDaysInFirstWeek();
+        System.out.println(minimalDaysInFirstWeek);
+        int i = instance.get(Calendar.WEEK_OF_MONTH);
+        System.out.println(i);
+        int i1 = instance.get(Calendar.WEEK_OF_YEAR);
+        System.out.println(i1);
+        instance.setFirstDayOfWeek(Calendar.MONDAY);
+        int i3 = instance.get(Calendar.DAY_OF_WEEK);
+        System.out.println(i3);
+        int i2 = instance.get(Calendar.DAY_OF_WEEK_IN_MONTH);
+        System.out.println(i2);
+
+        int i4 = instance.get(Calendar.DAY_OF_WEEK);
+        System.out.println(i4);
+
     }
 
 }
